4774692e2198ef4db43f3c12dd9d8b760cba7529
Christian Burrell <christian.burrell@gmail.com>
2022-12-16 18:03:03 -0500

    new commit

--- twitter_sentiment_analysis.py
+++ twitter_sentiment_analysis.py
@@ -1,4 +1,6 @@
 from textblob import TextBlob
+import spacy
+import asent
 import tweepy
 import sys
 from api_keys.secret import api_key, secret_api_key, access_token, secret_access_token
@@ -8,16 +10,27 @@ auth_handler.set_access_token(access_token, secret_access_token)
 
 api = tweepy.API(auth_handler)
 
-search_term = 'covid -filter:retweets -filter:replies'
+#add search term and amount of tweets you want to recieve
+search_term = '' # if you don't want retweets or replies: -filter:retweets -filter:replies
 tweet_amount = 10
 
 tweets = tweepy.Cursor(api.search_tweets, q=search_term, lang = 'en').items(tweet_amount)
 
-polarity = 0
+textblob_polarity = 0
+tweet_list = ''
+
+nlp = spacy.blank('en')
+nlp.add_pipe('sentencizer')
+nlp.add_pipe("asent_en_v1")
 
 for tweet in tweets:
     tweet = tweet.text
-    analysis = TextBlob(tweet)
-    polarity += analysis.polarity
+    textblob_analysis = TextBlob(tweet)
+    textblob_polarity += textblob_analysis.polarity
 
-print(polarity)
+    tweet_list += tweet
+    asent_analysis = nlp(tweet_list)
+    asent_polarity = asent_analysis._.polarity
+
+print(f"This is textblob's polarity: {textblob_polarity}")
+print(f"This is asent's polarity: {asent_polarity}")
