476524cbd817567e8d152991edc93e5991ba38d1
Christian Burrell <christian.burrell@gmail.com>
2022-12-20 12:28:11 -0500

    new commit

--- twitter_sentiment_analysis.py
+++ twitter_sentiment_analysis.py
@@ -2,7 +2,8 @@ from textblob import TextBlob
 import spacy
 import asent
 import tweepy
-import sys
+import re
+from datetime import datetime
 from api_keys.secret import api_key, secret_api_key, access_token, secret_access_token
 
 auth_handler = tweepy.OAuthHandler(consumer_key = api_key, consumer_secret = secret_api_key)
@@ -10,13 +11,15 @@ auth_handler.set_access_token(access_token, secret_access_token)
 
 api = tweepy.API(auth_handler)
 
+'''Editable Queries'''
 #add search term and amount of tweets you want to recieve
 search_term = '' # if you don't want retweets or replies: -filter:retweets -filter:replies
 tweet_amount = 10
+start = ''
+end = ''
 
-tweets = tweepy.Cursor(api.search_tweets, q=search_term, lang = 'en').items(tweet_amount)
+tweets = tweepy.Cursor(api.search_tweets, q=search_term, lang = 'en', tweet_mode = "extended").items(tweet_amount)
 
-textblob_polarity = 0
 tweet_list = ''
 
 nlp = spacy.blank('en')
@@ -24,11 +27,17 @@ nlp.add_pipe('sentencizer')
 nlp.add_pipe("asent_en_v1")
 
 for tweet in tweets:
-    tweet = tweet.text
-    textblob_analysis = TextBlob(tweet)
-    textblob_polarity += textblob_analysis.polarity
+    tweet = tweet.full_text
+    
+    replacements = ['http\S+', '#\S+', '@\S+']
+    for replacement in replacements:
+        tweet = re.sub(replacement, '', tweet, flags = re.IGNORECASE)
 
     tweet_list += tweet
+
+    textblob_analysis = TextBlob(tweet_list)
+    textblob_polarity = textblob_analysis.polarity
+
     asent_analysis = nlp(tweet_list)
     asent_polarity = asent_analysis._.polarity
 
